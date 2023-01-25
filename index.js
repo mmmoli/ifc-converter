c1fd769b46af78df91c5afa02b87e551ff678691
Michele Memoli <michele@100shapes.com>
2023-01-25 12:17:48 +0000

    Moved some thigns around

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
-"use strict";
+#!/usr/bin/env node
+const { program } = require("commander");
 
-const path = require("path"),
-    fs = require('fs'),
-    os = require('os'),
-    execFile = require("child_process").execFile;
+program
+  .name("IFC-Converter")
+  .description("CLI to convert IFC to other formats")
+  .version("0.1.0");
 
-const extractError = (output) => {
-    return output.split('\n')
-        .filter(s => s.startsWith('[Error]'))
-        .toString()
-        .replaceAll('[Error]', '');
-}
+program.parse(process.argv);
 
-module.exports = function ifcConvert(source, dest) {
-
-    return new Promise(function(resolve, reject) {
-
-        let ifcConvertPath;
-
-        switch (os.platform()) {
-            case "win32":
-                ifcConvertPath = path.resolve(__dirname, 'bin/ifcConvert-win.exe');
-                break;
-            case "darwin":
-                ifcConvertPath = path.resolve(__dirname, 'bin/ifcConvert-darwin');
-                break;
-            default:
-                ifcConvertPath = path.resolve(__dirname, 'bin/ifcConvert');
-                break;
-        }
-
-        if (fs.existsSync(source)) {
-            const process = execFile(ifcConvertPath, [source, dest], (e, stdout, stderr) => {
-                if (e) {
-                    if (stderr) {
-                        e = extractError(stderr.toString());
-                    } else {
-                        e = extractError(stdout.toString());
-                    }
-
-                    reject(new Error(e));
-                } else if (stdout.toString().includes("[Error]")) {
-                    reject(new Error(extractError(stdout)));
-                } else {
-                    resolve();
-                }
-            });
-
-            process.stdin.end();
-        } else {
-            reject(new Error('Cannot find file.'));
-        }
-    });
-};
+console.log(program.args);
